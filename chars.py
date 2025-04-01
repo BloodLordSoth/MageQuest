7ad0394fbceff480926a17bc15c67ae711a20857
BloodLordSoth <allhailsoth@gmail.com>
2025-03-31 21:50:31 -0400

    Third combat update

--- chars.py
+++ chars.py
@@ -1,4 +1,5 @@
 from weapons import fists
+from health_bar import HealthBar
 
 class Character:
 
@@ -12,6 +13,7 @@ class Character:
     def attack(self, target) -> None:
         target.health -= self.weapon.damage
         target.health = max(target.health, 0)
+        target.health_bar.update()
         print(f"{self.name} has dealt {self.weapon.damage} damage to "
               f"{target.name} with {self.weapon.name} ")
 
@@ -20,6 +22,7 @@ class Hero(Character):
         super().__init__(name=name, health=health)
 
         self.default_weapon = self.weapon
+        self.health_bar = HealthBar(self, color="green")
 
     def equip(self, weapon) -> None:
         self.weapon = weapon
@@ -35,5 +38,6 @@ class Enemy(Character):
     def __init__(self, name: str, health: int, weapon) -> None:
         super().__init__(name=name, health=health)
         self.weapon = weapon
+        self.health_bar = HealthBar(self, color="red")
 
 
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from chars import Hero, Enemy
 from weapons import short_bow, iron_sword
 
@@ -6,10 +7,11 @@ hero.equip(iron_sword)
 enemy = Enemy(name="Enemy", health = 100, weapon=short_bow)
 
 while True:
+    os.system("clear")
     hero.attack(enemy)
     enemy.attack(hero)
 
-    print(f"Health of {hero.name}: {hero.health}")
-    print(f"Health of {enemy.name}: {enemy.health}")
+    hero.health_bar.draw()
+    enemy.health_bar.draw()
 
     input()
