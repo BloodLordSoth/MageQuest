5e7e4de6f7a9eb14b8211790ab8c2cc8661c0ac1
BloodLordSoth <allhailsoth@gmail.com>
2025-03-31 20:28:37 -0400

    character class, and combat function added

---
+++ chars.py
@@ -0,0 +1,14 @@
+from weapons import fists
+
+class Character:
+
+    def __init__(self, name: str, health: int) -> None:
+        self.name = name
+        self.health = health
+        self.health_max = health
+        self.weapon = fists
+
+    
+    def attack(self, target) -> None:
+        target.health -= self.weapon.damage
+        target.health = max(target.health, 0)
